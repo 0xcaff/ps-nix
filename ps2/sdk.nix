9bd0bfe8e07812a7a8265ac275f2ade14334a6be
martin <martincharles07@gmail.com>
2025-04-13 11:05:45 -0500

    fix shell

--- ps2/sdk.nix
+++ ps2/sdk.nix
@@ -87,7 +87,14 @@ flake-utils.lib.eachSystem supported-systems (
   in
   {
     packages = flake-utils.lib.flattenTree {
-      ps2dev = pkgs.runCommand "ps2dev" {} ''
+      ps2dev = pkgs.runCommand "ps2dev" {
+        setupHook = pkgs.writeText "setup-hook" ''
+          export PS2DEV=@out@
+          export PS2SDK=$PS2DEV/ps2sdk
+          export GSKIT=$PS2DEV/gsKit
+          export PATH=$PATH:$PS2DEV/bin:$PS2DEV/ee/bin:$PS2DEV/iop/bin:$PS2DEV/dvp/bin:$PS2SDK/bin
+        '';
+      } ''
         mkdir -p $out
         ln -s ${dvp} $out/dvp
         ln -s ${iop} $out/iop
--- ps2/shell.nix
+++ ps2/shell.nix
@@ -5,11 +5,6 @@
       pkgs = import nixpkgs { system = "x86_64-linux"; };
     in
     pkgs.mkShell {
-      shellHook = ''
-        export PS2DEV=${self.packages.x86_64-linux.ps2dev}
-        export PS2SDK=$PS2DEV/ps2sdk
-        export GSKIT=$PS2DEV/gsKit
-        export PATH=$PATH:$PS2DEV/bin:$PS2DEV/ee/bin:$PS2DEV/iop/bin:$PS2DEV/dvp/bin:$PS2SDK/bin
-      '';
+      packages = [ self.packages.${pkgs.system}.ps2dev ];
     };
 }
