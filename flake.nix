e0217869121fbed35eba0edbe1e57904ad15dd0b
martin <martincharles07@gmail.com>
2025-04-16 15:58:31 -0500

    add public key

--- flake.nix
+++ flake.nix
@@ -3,6 +3,9 @@
     extra-substituters = [
       "https://ps-nix.cache.0xcaff.xyz"
     ];
+    extra-trusted-public-keys = [
+      "github-actions-ps-nix:1W9iqs0qtz9N08qX3TXeSWPuMycMUDa8u2EOBAvmuFE="
+    ];
   };
 
   description = "a collection of tools for ps homebrew development";
